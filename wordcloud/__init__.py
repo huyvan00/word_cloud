1e37c26e79c2927d1c9f824549ac62d949ba6bcf
van.bnh <bnhuyvan0505@gmail.com>
2021-05-10 15:11:39 +0700

    init image icon

--- wordcloud/__init__.py
+++ wordcloud/__init__.py
@@ -1,5 +1,6 @@
 from .wordcloud import (WordCloud, STOPWORDS, random_color_func,
                         get_single_color_func)
+from .image_cloud import (ImageCloud)
 from .color_from_image import ImageColorGenerator
 
 __all__ = ['WordCloud', 'STOPWORDS', 'random_color_func',
